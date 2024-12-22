b0a1fded70d4d5f5bd0405cee89caa3f9ec24819
MiguelDaehn <migueldaehnribeiro@gmail.com>
2024-12-22 18:04:51 -0300

    Docstrings - solvers

--- pysasf/solvers.py
+++ pysasf/solvers.py
@@ -15,6 +15,28 @@ To solve the overdetermined linear system of equations
 by least squares method
 '''  
 def solve_gls_4x4(y,X,S_inv):
+    '''
+    Solves the generalized least squares (GLS) problem for a 4x4 system of equations.
+    Purpose: to solve the overdetermined linear system of equations by generalized least squares method
+
+    Parameters
+    --------------------
+    y : numpy.ndarray
+        A 1D numpy array containing the selected Y (target) values.
+    X : numpy.ndarray
+        A 2D numpy array containing the sources' selected proportions values.
+    S_inv : numpy.ndarray
+        A 2D numpy array containing the inverse of the covariance matrix.
+
+    Returns
+    --------------------
+    numpy.ndarray
+        A 1D numpy array containing the estimated parameters of the GLS model.
+
+    Raises
+    --------------------
+    None
+    '''
     #print (X.shape) #7x3
     #print(S_inv.shape) #7x7
     B = S_inv.dot(X)#7x3
@@ -30,6 +52,25 @@ def solve_gls_4x4(y,X,S_inv):
     return (P)
 
 def solve_ols_cm(y,X):
+    '''
+    Solves the ordinary least squares (OLS) problem with constant mean (CM) constraints.
+
+    Parameters
+    --------------------
+    y : numpy.ndarray
+        A 1D numpy array containing the selected Y (target) values.
+    X : numpy.ndarray
+        A 2D numpy array containing the sources' selected proportions values.
+
+    Returns
+    --------------------
+    numpy.ndarray
+        A 1D numpy array containing the estimated parameters of the OLS-CM model, excluding the constant term.
+
+    Raises
+    --------------------
+    None
+    '''
     X = np.divide(X,y.reshape(len(X),1))
     A = X.T@X
     A = np.hstack((A, np.ones((len(A),1))))
@@ -53,6 +94,25 @@ def solve_ols_cm(y,X):
 
 
 def solve_ols(y,X):
+    '''
+    Solves the ordinary least squares (OLS) problem.
+
+    Parameters
+    --------------------
+    y : numpy.ndarray
+        A 1D numpy array containing the selected Y (target) values.
+    X : numpy.ndarray
+        A 2D numpy array containing the sources' selected proportions values.
+
+    Returns
+    --------------------
+    numpy.ndarray
+        A 1D numpy array containing the estimated parameters of the OLS model.
+
+    Raises
+    --------------------
+    None
+    '''
     X = np.divide(X,y.reshape(len(X),1))
     y = np.ones(len(X)) 
     P = np.linalg.inv(X.T@X)@(X.T@y)
@@ -60,6 +120,25 @@ def solve_ols(y,X):
 
 
 def solve_minimize(y,A):
+    '''
+    Solves an optimization problem using the scipy.optimize.minimize function.
+
+    Parameters
+    --------------------
+    y : numpy.ndarray
+        A 1D numpy array containing the selected Y (target) values.
+    X : numpy.ndarray
+        A 2D numpy array containing the sources' selected proportions values.
+
+    Returns
+    --------------------
+    numpy.ndarray
+        A 1D numpy array containing the estimated parameters of the optimization problem.
+
+    Raises
+    --------------------
+    None
+    '''
     from scipy.optimize import minimize
         
     #A = np.array([d.T ,e.T, l.T]).T
@@ -67,10 +146,44 @@ def solve_minimize(y,A):
     P0 = np.array([0.3, 0.3, 0.3])
     X0 = -np.log((1./P0)-1.)
         
-    def f(P):   
+    def f(P):
+        '''
+        Objective function to be minimized.
+
+        Parameters
+        --------------------
+        P: numpy.ndarray
+            A 1D numpy array containing the parameters to be optimized.
+
+        Returns
+        --------------------
+        float
+            The value of the objective function.
+
+        Raises
+        --------------------
+        None
+        '''
         return sum(((y-np.dot(A,P))/y)**2)
     
     def con(P):
+        '''
+        Constraint function for the optimization problem.
+
+        Parameters
+        --------------------
+        P: numpy.ndarray
+            A 1D numpy array containing the parameters to be optimized.
+
+        Returns
+        --------------------
+        float
+            The value of the constraint function.
+
+        Raises
+        --------------------
+        None
+        '''
         return P.sum()-1.0
        
     cons = ({'type': 'eq', 'fun': con})
@@ -84,6 +197,25 @@ def solve_minimize(y,A):
     return(P)
 
 def solve_minimize2(y,A):
+    """
+    Solves a constrained error minimization problem using the SciPy `minimize` function.
+
+    Parameters
+    ----------
+    y : numpy.ndarray
+        A 1D numpy array containing the selected Y (target) values.
+    A : numpy.ndarray
+        A 2D numpy array containing the sources' selected proportions values.
+
+    Returns
+    -------
+    numpy.ndarray
+        The optimal possible proportions solution vector.
+
+    Raises
+    ------
+    None
+    """
     from scipy.optimize import minimize, Bounds, LinearConstraint
     #https://docs.scipy.org/doc/scipy/tutorial/optimize.html#constrained-minimization-of-multivariate-scalar-functions-minimize
         
