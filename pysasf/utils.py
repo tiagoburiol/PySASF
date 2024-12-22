14cd13d0b3f3eeadbecb31cf06725ebbd581f363
MiguelDaehn <migueldaehnribeiro@gmail.com>
2024-12-22 17:28:08 -0300

    Update utils.py

--- pysasf/utils.py
+++ pysasf/utils.py
@@ -16,23 +16,23 @@ import numpy as np
 #https://learner-cares.medium.com/handy-python-pandas-for-data-normalization-and-scaling-9658846de8fc
 def minmax_scale(bd):
     """
-        This function is currently not in use.
-        Scale the features of a dataset to the range [0, 1] using Min-Max scaling.
+    This function is currently not in use.
+    Scale the features of a dataset to the range [0, 1] using Min-Max scaling.
 
-        Parameters
-        --------------------
-        bd : object
-            An object containing a dictionary of DataFrames in its `df_dict` attribute.
+    Parameters
+    --------------------
+    bd : object
+        An object containing a dictionary of DataFrames in its `df_dict` attribute.
 
-        Returns
-        --------------------
-        bd_n : object
-            A new object with the same structure as `bd`, but with the DataFrames in `df_dict` scaled to the range [0, 1] using Min-Max scaling.
+    Returns
+    --------------------
+    bd_n : object
+        A new object with the same structure as `bd`, but with the DataFrames in `df_dict` scaled to the range [0, 1] using Min-Max scaling.
 
-        Raises
-        --------------------
-        None
-        """
+    Raises
+    --------------------
+    None
+    """
     from sklearn.preprocessing import MinMaxScaler
     import copy
     
